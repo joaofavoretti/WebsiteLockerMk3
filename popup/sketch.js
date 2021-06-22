b215da4473429833a2ffcea96ffc48473e988992
Joao Pedro Favoretti <joaopedrofavoretti@gmail.com>
2021-06-22 18:44:55 -0300

    removing skip

--- popup/sketch.js
+++ popup/sketch.js
@@ -3,20 +3,12 @@ async function setup() {
   noCanvas();
 
   const disable_button = document.getElementById('disable_button');
-  const disable_skip_button = document.getElementById('disable_skip_button');
   const clear_blacklist = document.getElementById('clear_blacklist');
 
   disable_button.addEventListener('click', async () => {
     const { disable_extension } = await new Promise((resolve) => chrome.storage.sync.get('disable_extension', (value) => resolve(value)));
     chrome.storage.sync.set({ disable_extension: !disable_extension }, () => {
       console.warn("setted", { disable_extension: !disable_extension })
-    });
-  });
-
-  disable_skip_button.addEventListener('click', async () => {
-    const { disable_skip } = await new Promise((resolve) => chrome.storage.sync.get('disable_skip', (value) => resolve(value)));
-    chrome.storage.sync.set({ disable_skip: !disable_skip}, () => {
-      console.warn("setted", { disable_skip: !disable_skip});
     });
   });
 
@@ -65,12 +57,7 @@ async function draw() {
   frameRate(10);
 
   const { disable_extension } = await new Promise((resolve) => chrome.storage.sync.get('disable_extension', (value) => resolve(value)));
-  const { disable_skip } = await new Promise((resolve) => chrome.storage.sync.get('disable_skip', (value) => resolve(value)));
 
   if (disable_extension) disable_button.textContent = "Enable Extension";
   else disable_button.textContent = "Disable Extension"
-  
-  if (disable_skip) disable_skip_button.textContent = "Enable Skip";
-  else disable_skip_button.textContent = "Disable Skip";
-
 }
